b58429f2ca16ac5637841d601ff846e1ce7ee31b
thederekjay <thederekjay@gmail.com>
2011-08-11 22:13:10 -0400

    reduce vfs_cache_pressure by 30%

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Thu Jun 13 23:10:30 EDT 2013"
+#define UTS_VERSION "#3 PREEMPT Thu Jun 13 23:43:10 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
