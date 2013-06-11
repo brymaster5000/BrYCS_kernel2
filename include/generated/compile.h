18197861a35693b0fcd1100d7447756d92d4f447
Bryan Maltais <brymaster5000@gmail.com>
2013-06-10 22:52:11 -0400

    added bfq ioscheduler

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Thu Jun 13 21:24:38 EDT 2013"
+#define LINUX_COMPILE_BY "brymaster5000"
+#define LINUX_COMPILE_HOST "bryslaptop"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
