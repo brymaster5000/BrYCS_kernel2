74f6b6a53b2c80a334513f25bf2f407f6c5dfd3d
Bryan Maltais <brymaster5000@gmail.com>
2013-06-13 23:52:48 -0400

    build status

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 4 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Thu Jun 13 23:43:10 EDT 2013"
+#define UTS_VERSION "#4 PREEMPT Thu Jun 13 23:52:06 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
