18cdd5e7b848ba363d0e9881644cc95fb361c512
Bryan Maltais <brymaster5000@gmail.com>
2013-06-14 22:25:40 -0400

    Revert "rcu:move TINY_RCU from softirq to kthread"
    This reverts commit 8a7b6c5b4dcc071fe20b1b63c06c777483c4efc1.

--- kernel/rcutiny.c
+++ kernel/rcutiny.c
@@ -53,15 +53,8 @@ static void __call_rcu(struct rcu_head *head,
 
 #include "rcutiny_plugin.h"
 
-/* Controls for rcu_cbs() kthread, replacing RCU_SOFTIRQ used previously. */
-static struct task_struct *rcu_cbs_task;
-static DECLARE_WAIT_QUEUE_HEAD(rcu_cbs_wq);
-static unsigned long have_rcu_cbs;
-static void invoke_rcu_cbs(void);
-
 /* Forward declarations for rcutiny_plugin.h. */
-static void rcu_process_callbacks(struct rcu_ctrlblk *rcp);
-static int rcu_cbs(void *arg);
+static void __rcu_process_callbacks(struct rcu_ctrlblk *rcp);
 static void __call_rcu(struct rcu_head *head,
 		       void (*func)(struct rcu_head *rcu),
 		       struct rcu_ctrlblk *rcp);
@@ -132,12 +125,8 @@ void rcu_sched_qs(int cpu)
 	local_irq_save(flags);
 	if (rcu_qsctr_help(&rcu_sched_ctrlblk) +
 	    rcu_qsctr_help(&rcu_bh_ctrlblk))
-<<<<<<< HEAD
 		invoke_rcu_kthread();
 	local_irq_restore(flags);
-=======
-		invoke_rcu_cbs();
->>>>>>> 22677b7... rcu: move TINY_RCU from softirq to kthread
 }
 
 /*
@@ -149,12 +138,8 @@ void rcu_bh_qs(int cpu)
 
 	local_irq_save(flags);
 	if (rcu_qsctr_help(&rcu_bh_ctrlblk))
-<<<<<<< HEAD
 		invoke_rcu_kthread();
 	local_irq_restore(flags);
-=======
-		invoke_rcu_cbs();
->>>>>>> 22677b7... rcu: move TINY_RCU from softirq to kthread
 }
 
 /*
@@ -204,11 +189,7 @@ static void rcu_process_callbacks(struct rcu_ctrlblk *rcp)
 		prefetch(next);
 		debug_rcu_head_unqueue(list);
 		local_bh_disable();
-<<<<<<< HEAD
 		__rcu_reclaim(list);
-=======
-		list->func(list);
->>>>>>> 22677b7... rcu: move TINY_RCU from softirq to kthread
 		local_bh_enable();
 		list = next;
 		RCU_TRACE(cb_count++);
@@ -222,7 +203,6 @@ static void rcu_process_callbacks(struct rcu_ctrlblk *rcp)
  * RCU_SOFTIRQ that was used previously for this purpose.
  * This is a kthread, but it is never stopped, at least not until
  * the system goes down.
-<<<<<<< HEAD
  */
 static int rcu_kthread(void *arg)
 {
@@ -250,41 +230,6 @@ static int rcu_kthread(void *arg)
 	__rcu_process_callbacks(&rcu_sched_ctrlblk);
 	__rcu_process_callbacks(&rcu_bh_ctrlblk);
 	rcu_preempt_process_callbacks();
-=======
- */
-static int rcu_cbs(void *arg)
-{
-	unsigned long work;
-	unsigned long flags;
-
-	for (;;) {
-		wait_event(rcu_cbs_wq, have_rcu_cbs != 0);
-		local_irq_save(flags);
-		work = have_rcu_cbs;
-		have_rcu_cbs = 0;
-		local_irq_restore(flags);
-		if (work) {
-			rcu_process_callbacks(&rcu_sched_ctrlblk);
-			rcu_process_callbacks(&rcu_bh_ctrlblk);
-			rcu_preempt_process_callbacks();
-		}
-	}
-
-	return 0;  /* Not reached, but needed to shut gcc up. */
-}
-
-/*
- * Wake up rcu_cbs() to process callbacks now eligible for invocation.
- */
-static void invoke_rcu_cbs(void)
-{
-	unsigned long flags;
-
-	local_irq_save(flags);
-	have_rcu_cbs = 1;
-	wake_up(&rcu_cbs_wq);
-	local_irq_restore(flags);
->>>>>>> 22677b7... rcu: move TINY_RCU from softirq to kthread
 }
 
 /*
@@ -391,7 +336,3 @@ early_initcall(rcu_spawn_kthreads);
 	open_softirq(RCU_SOFTIRQ, rcu_process_callbacks);
 }
 
-	rcu_cbs_task = kthread_run(rcu_cbs, NULL, "rcu_cbs");
-	return 0;
-}
-early_initcall(rcu_spawn_kthreads);
