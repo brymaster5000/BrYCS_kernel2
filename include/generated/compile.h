c4f252139a31d1c498fc9f011298bbe82fd48c91
Bryan Maltais <brymaster5000@gmail.com>
2013-06-14 23:54:45 -0400

    rcu: add tracing data to support queueing models

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 13 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 PREEMPT Fri Jun 14 23:33:51 EDT 2013"
+#define UTS_VERSION "#13 PREEMPT Fri Jun 14 23:52:57 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
