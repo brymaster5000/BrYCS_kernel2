fdeb2e298e7b8c87db4d23167d164de113b288f8
Bryan Maltais <brymaster5000@gmail.com>
2013-06-20 21:16:10 -0400

    fixed cpufreq Kconfig, added Zen ioscheduler, and added gpu oc

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 24 */
+/* This file is auto generated, version 26 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#24 PREEMPT Tue Jun 18 22:07:56 EDT 2013"
+#define UTS_VERSION "#26 PREEMPT Thu Jun 20 21:12:07 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
