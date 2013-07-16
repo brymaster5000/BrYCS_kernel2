3a2e284fb2e2c607ff6800a5038393438b3c6391
Bryan Maltais <brymaster5000@gmail.com>
2013-07-16 19:40:26 -0400

    trying to make it boot

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 29 */
+/* This file is auto generated, version 33 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#29 PREEMPT Tue Jul 9 23:43:05 EDT 2013"
+#define UTS_VERSION "#33 PREEMPT Thu Jul 11 20:31:03 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
