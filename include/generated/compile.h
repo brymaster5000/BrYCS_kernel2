3f10deff15fa2bc3b2b90683b612ad56113d4af8
Bryan Maltais <brymaster5000@gmail.com>
2013-07-16 21:54:44 -0400

    attempts

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 33 */
+/* This file is auto generated, version 36 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#33 PREEMPT Thu Jul 11 20:31:03 EDT 2013"
+#define UTS_VERSION "#36 PREEMPT Tue Jul 16 21:34:04 EDT 2013"
 #define LINUX_COMPILE_BY "brymaster5000"
 #define LINUX_COMPILE_HOST "bryslaptop"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
